b579296ae5733fc0a351a149812935813fb17d71
Syed Iftekharuddin <syeydy636@gmail.com>
2025-04-09 09:56:49 +0200

    Function mini project for authentication without DB added

---
+++ basic-auth-no-db/src/main/java/com/app/basic_auth_no_db/security/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.app.basic_auth_no_db.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity // TODO what this does
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+            http
+                .authorizeHttpRequests( auth -> auth
+                        .anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults());
+            return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

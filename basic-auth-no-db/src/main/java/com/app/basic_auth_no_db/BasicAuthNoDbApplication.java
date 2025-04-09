60c8a41e4af9fd785fd224aff344d81036301048
Syed Iftekharuddin <syeydy636@gmail.com>
2025-04-09 08:07:59 +0200

    New module for basic authentication with no DB added

---
+++ basic-auth-no-db/src/main/java/com/app/basic_auth_no_db/BasicAuthNoDbApplication.java
@@ -0,0 +1,13 @@
+package com.app.basic_auth_no_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthNoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthNoDbApplication.class, args);
+	}
+
+}

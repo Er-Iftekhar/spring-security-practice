2fcba7bb9b39281cec9b87232c46f1e08844a029
Syed Iftekharuddin <syeydy636@gmail.com>
2026-01-28 16:58:14 +0100

    Initial project set up

---
+++ auth-ad-demo/src/main/java/com/app/auth_ad_demo/AuthAdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.app.auth_ad_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAdDemoApplication.class, args);
+	}
+
+}
---
+++ auth-ad-demo/src/test/java/com/app/auth_ad_demo/AuthAdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.auth_ad_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

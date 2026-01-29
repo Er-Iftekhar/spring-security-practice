140613bab770cc9f83daaf4ae4ee90976a2b17ee
Syed Iftekharuddin <syeydy636@gmail.com>
2026-01-29 18:58:09 +0100

    Initial project add

---
+++ auth-ldap-demo2/src/main/java/com/app/auth_ldap_demo2/AuthLdapDemo2Application.java
@@ -0,0 +1,13 @@
+package com.app.auth_ldap_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthLdapDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthLdapDemo2Application.class, args);
+	}
+
+}
